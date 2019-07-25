3ef03df2d752bb1671785f7e9678ccb3cb72427f
unknown <jjamal95@gmail.com>
2019-07-24 20:32:56 -0500

    server up and running

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require('express');
+const port = 5000;
+
+const server = express();
+server.listen(5000, () => {
+    console.log(`server listening on port ${port}`);
+});
+
+
+
+
+// GET 
+server.get('/', (req, res) => {
+    res.send('Hello World from Express!');
+} );
+
